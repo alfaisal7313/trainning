638e4a39206acab42213cca6298aa150804235ef
faisalaja <fais.season2@gmail.com>
2018-02-03 22:30:28 +0700

    fix bug

--- MyApp/app/src/main/java/com/example/dev/myapp/view/activities/MainActivity.java
+++ MyApp/app/src/main/java/com/example/dev/myapp/view/activities/MainActivity.java
@@ -1,21 +1,18 @@
 package com.example.dev.myapp.view.activities;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
 import com.example.dev.myapp.R;
 
-/**
- * Created by MacBook on 2/3/18.
- */
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
 
         Toolbar toolbar = findViewById(R.id.toolbar_main);
         toolbar.setTitle(getResources().getString(R.string.app_name));
@@ -24,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private int getStatusBarHeight() {
-        final int resourceId = getResources().getIdentifier("status_bar_hight", "dimen", "android");
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
         return resourceId > 0 ? getResources().getDimensionPixelOffset(resourceId) : 0;
     }
 }
